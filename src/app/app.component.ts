666d6df02505ad859fe34e15ad45437a356ac2a2
PritishKhobragade <pritishkhobragade1234@gmail.com>
2026-06-30 22:17:22 +0530

    feat: implement role selection and employee listing
    - Added a dropdown for selecting employee roles in app.component.html.
    - Implemented dynamic display of employee lists based on selected role using NgSwitch.
    - Updated app.component.ts to include employee data and manage selected role state with signals.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { NgForOf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface empInterface {
+  id: number;
+  name: string;
+  role: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule, NgForOf, NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngFor';
+  employeeRole: string = '';
+
+  employees = signal<empInterface[]>([
+    { id: 1, name: 'EmployeeA', role: 'Admin' },
+    { id: 2, name: 'EmployeeB', role: 'HR' },
+    { id: 3, name: 'EmployeeC', role: 'Marketing' },
+    { id: 4, name: 'EmployeeD', role: 'Developer' },
+    { id: 5, name: 'EmployeeE', role: 'Marketing' },
+    { id: 6, name: 'EmployeeF', role: 'Marketing' },
+    { id: 7, name: 'EmployeeG', role: 'Analyst' }
+  ]);
 }
+
